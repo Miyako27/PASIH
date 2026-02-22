190321a8a5864d5ac18b9d85fa28d669d62be4d7
Miyako <miyako.otsu@gmail.com>
2026-02-22 11:51:28 +0700

    update badge role

--- resources/views/pages/admin/instansi/index.blade.php
+++ resources/views/pages/admin/instansi/index.blade.php
@@ -70,6 +70,9 @@
                     <a href="{{ route('admin.instansi.show', $institution) }}" class="h-8 w-8 rounded-md bg-blue-600 text-white inline-flex items-center justify-center" title="Detail">
                       <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" /><circle cx="12" cy="12" r="3" /></svg>
                     </a>
+                    <a href="{{ route('admin.instansi.edit', $institution) }}" class="h-8 w-8 rounded-md bg-amber-400 text-white inline-flex items-center justify-center" title="Edit">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5M16.5 3.5a2.121 2.121 0 113 3L12 14l-4 1 1-4 7.5-7.5z" /></svg>
+                    </a>
                     <form method="POST" action="{{ route('admin.instansi.destroy', $institution) }}" data-confirm-type="delete" data-confirm-message="Apakah Anda yakin ingin menghapus data ini?">
                       @csrf
                       @method('DELETE')
@@ -77,9 +80,6 @@
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-1 12a2 2 0 01-2 2H8a2 2 0 01-2-2L5 7m5 4v6m4-6v6M9 7V4a1 1 0 011-1h4a1 1 0 011 1v3M4 7h16" /></svg>
                       </button>
                     </form>
-                    <a href="{{ route('admin.instansi.edit', $institution) }}" class="h-8 w-8 rounded-md bg-amber-400 text-white inline-flex items-center justify-center" title="Edit">
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5M16.5 3.5a2.121 2.121 0 113 3L12 14l-4 1 1-4 7.5-7.5z" /></svg>
-                    </a>
                   </div>
                 </td>
               </tr>
