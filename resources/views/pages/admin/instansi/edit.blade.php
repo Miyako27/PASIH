9875916a24afa78d819bbfd25667d8db312463d3
Miyako <miyako.otsu@gmail.com>
2026-05-01 12:11:15 +0700

    ubah button simpan saat edit di admin

--- resources/views/pages/admin/instansi/edit.blade.php
+++ resources/views/pages/admin/instansi/edit.blade.php
@@ -51,7 +51,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a2 2 0 002 2h12a2 2 0 002-2v-1M12 4v12m0 0l-4-4m4 4l4-4" />
             </svg>
-            Simpan
+            Simpan Perubahan
           </button>
         </div>
       </form>
