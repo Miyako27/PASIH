2e24639576fd75a1ae79a5daa226bfec3b969f6a
Miyako <miyako.otsu@gmail.com>
2026-05-03 22:11:37 +0700

    ubah ukuran button penugasan permohonan

--- resources/views/pages/submissions/penugasan.blade.php
+++ resources/views/pages/submissions/penugasan.blade.php
@@ -53,7 +53,7 @@
         </label>
 
         <div>
-          <button type="submit" class="inline-flex w-full sm:w-auto justify-center items-center gap-2 h-11 px-5 rounded-md bg-emerald-600 text-white text-sm font-semibold hover:bg-emerald-700">
+          <button type="submit" class="inline-flex w-full sm:w-auto justify-center items-center gap-2 h-10 px-4 rounded-md bg-emerald-600 text-white text-sm font-semibold hover:bg-emerald-700">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a2 2 0 002 2h12a2 2 0 002-2v-1M12 4v12m0 0l-4-4m4 4l4-4" />
             </svg>
